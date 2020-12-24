7a1efd1c70284fe3048113c8d06ebef7d3745707
James <67014970+JLoweDev@users.noreply.github.com>
2020-12-24 13:53:29 +0000

    basic index.js finished with event listeners for nav buttons

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import loadPage from './modules/init';
+import loadHome from './modules/home';
+import loadMenu from './modules/menu';
+import loadContact from './modules/menu';
+
+function addNavEvents() {
+    const menuBtn = document.getElementById('menu');
+    const aboutBtn = document.getElementById('about');
+    const contactBtn = document.getElementById('contact');
+    menuBtn.addEventListener('click', loadMenu);
+    aboutBtn.addEventListener('click', loadHome);
+    contactBtn.addEventListener('click', loadContact);
+}
+
+function init() {
+    loadPage();
+    loadHome();
+    addNavEvents();
+}
+
+//init();
